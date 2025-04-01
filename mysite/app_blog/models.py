14fc01cb7218f47a4fa0e15f56c79660d51e52a0
slobodianiukdenys <144223224+slobodianiukdenys@users.noreply.github.com>
2025-04-01 09:49:49 +0300

    Add files via upload

--- mysite/app_blog/models.py
+++ mysite/app_blog/models.py
@@ -3,18 +3,32 @@ from django.utils import timezone
 from django.db import models
 from django.urls import reverse
 
-
-
 class Category(models.Model):
-    category = models.CharField(u'Категорія',max_length=250, help_text=u'Максимум 250 символів')
+    category = models.CharField(
+        u'Категорія',
+        max_length=250,
+        help_text=u'Максимум 250 сим.'
+    )
     slug = models.SlugField(u'Слаг')
+    objects = models.Manager()
 
     class Meta:
-        verbose_name = u'Статя'
-        verbose_name_plural = u'Статті'
+        verbose_name = u'Категорія для публікації'
+        verbose_name_plural = u'Категорії для публікацій'
 
     def __str__(self):
         return self.category
+
+    def get_absolute_url(self):
+        try:
+            url = reverse(
+                'articles-category-list',
+                kwargs={'slug': self.slug}
+            )
+        except:
+            url = "/"
+        return url
+
     
 
 class Article(models.Model):
